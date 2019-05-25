f03654a4e651c6dd0ae54675c87235c5edfd3239
moelmicrosyste <51031445+moelmicrosyste@users.noreply.github.com>
2019-05-25 18:50:58 +0000

    Create tv.js

---
+++ tv.js
@@ -0,0 +1,110 @@
+var c=0;
+function show(){
+    c ++;
+if(c==1){
+document.getElementById("menu").style.right="0px";
+       }
+if(c==2){
+document.getElementById("menu").style.right="-220px";
+c=0;
+    
+}   
+}
+
+          function aljazera(){
+             document.getElementById("iframe").style.visibility="visible";
+             
+              document.getElementById("iframe").src="https://www.youtube-nocookie.com/embed/live_stream?channel=UCfiwzLy-8yKzIbsmZTzxDgw&amp;modestbranding=1";
+          }
+             function aljazeradoc(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube.com/embed/fZKkK-qEEng";
+          }
+            function bbc(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube.com/embed/HxI2TxhhS9A";
+          }
+            
+            function almayaden(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube-nocookie.com/embed/live_stream?channel=UCuJDz6R_lHaxeGnFTnv3tjg&amp;modestbranding=1";
+          }
+            
+            function france24(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube-nocookie.com/embed/live_stream?channel=UCdTyuXgmJkG_O8_75eqej-w&amp;modestbranding=1" ;
+          }
+            
+            function telemaroc(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://livestream.com/accounts/27130247/events/8196478/player?width=100&height=540&enableInfoAndActivity=true&defaultDrawer=feed&autoPlay=true&mute=false";
+          }
+            
+            function bnnews(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube.com/embed/xbxDuKqV61Y";
+          }
+            
+        function noon(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube.com/embed/XQfwl2lwA1Q?autoplay=1";
+          }
+            
+            function arriyadia(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://yalla-shoot.com/ch/arryadia.php";
+          }
+            
+            function alaoula(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.casasat.com/tv/aloula_inclu.htm";
+          }
+             function tamazight(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://yalla-shoot.com/ch/tamazight.php";
+          }
+             function adsport1(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.yalla-shoot.com/ch/ch21ad.php";
+          }
+             function adsport2(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.yalla-shoot.com/ch/ch22ad.php";
+          }
+            function bnsporthd(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://yalla6.xyz/goals/youtube.php?ytid=SoUzfMJz-54";
+          }
+          
+             function makah(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://oui9mobi.com/pyr/fplyr1.php?id=http://makkahtv.srfms.com:1935/makkahtv/livestream/chunklist_w966623075.m3u8";
+          }
+              function karamech(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube.com/embed/E4Is2JuRUCs";
+          }
+             function ayat(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://oui9mobi.com/pyr/fplyr2.php?id=http://m.live.net.sa:1935/live/ayat/chunklist_w2078329880.m3u8";
+          }
+             function quest(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=questarabiya";
+          }
+             function alarabiya(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="https://www.youtube-nocookie.com/embed/BEOtIfN3t64";
+          }
+            function alhora(){
+            document.getElementById("iframe").style.visibility="visible";
+              document.getElementById("iframe").src="https://www.youtube-nocookie.com/embed?max-results=5&controls=1&showinfo=1&rel=0&listType=user_uploads&list=alhurra";
+          }
+            function dm(){
+            document.getElementById("iframe").style.visibility="visible";
+              document.getElementById("iframe").src="            https://www.elahmad.com/tv/watch.php?id=2m_monde";
+          }
+            function arrabia(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=arrabia";
+          }
+             function md1(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=medi1tv";
+          }
+            function almaghribiya(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=al_maghribia";
+          }
+             function assadissa(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=assadissatv";
+          }
+            function mekamelen(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=mekameleen_tv1";
+          }
+           
+            function natgeo(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="http://www.elahmad.com/tv/watch.php?id=natgeo_2";
+          }
+              function canal(){
+             document.getElementById("iframe").style.visibility="visible"; document.getElementById("iframe").src="            https://playtv.fr/player/embed/canal-algerie/";
+          }   
